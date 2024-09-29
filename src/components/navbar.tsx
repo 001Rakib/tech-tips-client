bc071eb048462aea7f1db0da3cf26b4a9300aeb1
001Rakib <rakibrakibrakib354@gmail.com>
2024-09-29 22:38:56 +0600

    initial project setup

---
+++ src/components/navbar.tsx
@@ -0,0 +1,63 @@
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+} from "@nextui-org/navbar";
+import { Button } from "@nextui-org/button";
+
+import { link as linkStyles } from "@nextui-org/theme";
+import NextLink from "next/link";
+import clsx from "clsx";
+
+import { siteConfig } from "@/src/config/site";
+import { ThemeSwitch } from "@/src/components/theme-switch";
+
+export const Navbar = () => {
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <p className="font-bold text-inherit">NextGenTechTips</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden lg:flex gap-4  ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium"
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          <ThemeSwitch />
+        </NavbarItem>
+
+        <NavbarItem className="hidden md:flex">
+          <Button>Login</Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <ThemeSwitch />
+        <NavbarMenuToggle />
+      </NavbarContent>
+    </NextUINavbar>
+  );
+};
